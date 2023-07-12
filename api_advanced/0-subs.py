fe120ea19ee46924c558ad57fda10171e9843a7e
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-07-12 18:44:40 +0000

    return number of subscribes

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""return number of subscribes"""
+import json
+import sys
+import urllib.request
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    req = urllib.request.Request(url, headers=headers)
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            data = response.read().decode('utf-8')
+            subscribers = json.loads(data).get("data", {}).get("subscribers", 0)
+            return subscribers
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            return 0
+        else:
+            print("Error: ", e.code)
+    except urllib.error.URLError as e:
+        print("Error: ", e.reason)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        result = number_of_subscribers(subreddit)
+        print(result)
