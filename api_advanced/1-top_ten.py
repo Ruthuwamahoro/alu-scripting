3f12571b852b76245da55576c7c263dbca668c42
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-07-13 16:05:21 +0000

    done

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,35 +1,22 @@
 #!/usr/bin/python3
-"""print the titles of the first 10 host posts"""
-import argparse
-import json
-import sys
-import urllib.request
+"""Contains top_ten function"""
+import requests
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    requesting = urllib.request.Request(url, headers=headers)
-    try:
-        with urllib.request.urlopen(requesting) as response:
-            data = json.loads(response.read().decode('utf-8'))
-            posts = data["data"]["children"]
-	  
-            for post in posts:
-                title = post["data"]["title"]
-                sys.stdout.buffer.write(title.encode(sys.stdout.encoding, errors='replace'))
-                sys.stdout.buffer.write(b'\n')
-          
-    except urllib.error.HTTPError as e:
-        if e.code == 404:
-            print("None")
-        else:
-            raise
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Get top 10 hot posts from a subreddit")
-    parser.add_argument("subreddit", type=str, help="Name of the subreddit")
-    args = parser.parse_args()
-
-    top_ten(args.subreddit)
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
